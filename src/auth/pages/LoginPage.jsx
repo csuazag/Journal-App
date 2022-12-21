f8fadb289f1ed62a27864722e13b54905e131854
Camilo Suaza <camilo.suaza12@gmail.com>
2022-12-20 22:37:26 -0500

    initial state, login, register page, and more pages

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,6 +1,53 @@
+import { Link as RouterLink } from "react-router-dom";
+import { Google } from "@mui/icons-material";
+import { Button, Grid, Link, TextField, Typography } from "@mui/material";
+import { AuthLayout } from "../layout/AuthLayout";
 
 export const LoginPage = () => {
   return (
-    <div>Login Page</div>
-  )
-}
+    <AuthLayout title="Login">
+      <form action="">
+        <Grid container>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="email"
+              type="email"
+              placeholder="email@google.com"
+              fullWidth
+            />
+          </Grid>
+
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="password"
+              type="password"
+              placeholder="email@google.com"
+              fullWidth
+            />
+          </Grid>
+
+          <Grid container spacing={2} sx={{ mt: 2, mb: 2 }}>
+            <Grid item xs={12} md={6}>
+              <Button variant="contained" fullWidth>
+                Login
+              </Button>
+            </Grid>
+
+            <Grid item xs={12} md={6}>
+              <Button variant="outlined" fullWidth>
+                <Google />
+                <Typography sx={{ ml: 1 }}>Google</Typography>
+              </Button>
+            </Grid>
+          </Grid>
+
+          <Grid container direction="row" justifyContent="end">
+            <Link component={RouterLink} color="inherit" to="/auth/signup">
+              Create an account
+            </Link>
+          </Grid>
+        </Grid>
+      </form>
+    </AuthLayout>
+  );
+};
--- src/auth/pages/SignupPage.jsx
+++ src/auth/pages/SignupPage.jsx
@@ -1,6 +1,52 @@
+import { Link as RouterLink } from "react-router-dom";
+
+import { Button, Grid, Link, TextField, Typography } from "@mui/material";
+import { AuthLayout } from "../layout/AuthLayout";
 
 export const SignupPage = () => {
   return (
-    <div>Signup Page</div>
-  )
-}
+    <AuthLayout title="Signup">
+      <form action="">
+        <Grid container>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Full Name"
+              type="text"
+              placeholder="Name"
+              fullWidth
+            />
+          </Grid>
+
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="password"
+              type="password"
+              placeholder="email@google.com"
+              fullWidth
+            />
+          </Grid>
+
+          <Grid container sx={{ mt: 2, mb: 2 }}>
+            <Grid item xs={12}>
+              <Button variant="contained" fullWidth>
+                Login
+              </Button>
+            </Grid>
+          </Grid>
+
+          <Grid container direction="row" justifyContent="end">
+            <Grid item xs={12}>
+              <Typography sx={{ mr: 1 }}>
+                Do you already have an account?
+              </Typography>
+
+              <Link component={RouterLink} color="inherit" to="/auth/login">
+                Login
+              </Link>
+            </Grid>
+          </Grid>
+        </Grid>
+      </form>
+    </AuthLayout>
+  );
+};
